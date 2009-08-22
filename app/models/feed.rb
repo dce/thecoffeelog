065a0f1e84efffbd67f5a9e42fc6dc2717825977
Ren Provey <ren.provey@gmail.com>
2009-08-21 22:27:44 -0400

    Fetching feed titles.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,9 +1,15 @@
 class Feed < ActiveRecord::Base
   validates_presence_of :url
-  
+
+  before_create :set_feed_title
+
   def url_from_user=(user_url)
     feeds = Feedbag.find(user_url)
     self.url = feeds.first
     self.errors.add :url, "contains no feeds"
+  end
+
+  def set_feed_title
+    self.title = FeedTools::Feed.open(self.url).title
   end
 end
--- test/unit/feed_test.rb
+++ test/unit/feed_test.rb
@@ -2,6 +2,18 @@ require 'test_helper'
 
 class FeedTest < ActiveSupport::TestCase
   context "A feed" do
+    setup do
+      @feed = Feed.create(:url_from_user => "http://www.nytimes.com/")
+    end
+
     should_validate_presence_of :url
+    
+    should "return a vaild feed from NY Times homepage url" do
+      assert_equal @feed.url, "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+    end
+    
+    should "return the NY Times homepage feed name" do
+      assert_equal @feed.title, "NYT &gt; Home Page"
+    end
   end
 end
