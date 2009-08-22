f54af1446956669a0b3efa089d432cdbbd2fe5f4
David Eisinger <david.eisinger@viget.com>
2009-08-22 17:34:02 -0400

    Sending mail as HTML

--- app/models/feed_mailer.rb
+++ app/models/feed_mailer.rb
@@ -1,8 +1,9 @@
 class FeedMailer < ActionMailer::Base
   def coffee_log(user)
-    recipients  user.email
-    from        "updates@thecoffeelog.com"
-    subject     "Your CoffeeLog for #{Date.today}"
-    body        :user => user
+    recipients    user.email
+    from          "updates@thecoffeelog.com"
+    subject       "Your CoffeeLog for #{Date.today}"
+    body          :user => user
+    content_type  "text/html"
   end
 end
