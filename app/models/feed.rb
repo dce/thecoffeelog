4703e812d78d61c4dbea6768a7fc309e70d2c8ba
David Eisinger <david.eisinger@viget.com>
2009-08-22 14:24:34 -0400

    Refactoring

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -16,23 +16,12 @@ class Feed < ActiveRecord::Base
   end
 
   def fetch_updates(limit = nil)
-    all_entries = feed_data.entries.map {|d| self.entries.from_feedtools(d) }
+    items = new_entries
+    items = items.first(limit) if limit
 
-    last_sent = all_entries.detect do |e|
-      e.generate_unique_key == self.last_sent_entry_hash
-    end
-
-    new_entries = if last_sent
-      all_entries[0, all_entries.index(last_sent)]
-    else
-      all_entries.select {|e| e.published_at > self. last_sent_entry_published_at }
-    end
-
-    new_entries = new_entries[0, limit] if limit
-
-    if new_entries.any?
-      new_entries.map(&:save)
-      new_entries.first.mark_as_last_sent
+    if items.any?
+      items.map(&:save)
+      items.first.mark_as_last_sent
     end
   end
 
@@ -57,5 +46,19 @@ class Feed < ActiveRecord::Base
 
   def set_feed_title
     self.title ||= feed_data.title
+  end
+
+  def new_entries
+    all_entries = feed_data.entries.map {|d| self.entries.from_feedtools(d) }
+
+    last_sent = all_entries.detect do |e|
+      e.generate_unique_key == self.last_sent_entry_hash
+    end
+
+    if last_sent
+      all_entries[0, all_entries.index(last_sent)]
+    else
+      all_entries.select {|e| e.published_at > self. last_sent_entry_published_at }
+    end
   end
 end
