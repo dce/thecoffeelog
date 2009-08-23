5dc05961c4fd9828090d3c660d422183646c0877
Ren Provey <ren.provey@gmail.com>
2009-08-23 09:40:30 -0400

    Renaming mail_queue_processor_control.rb for clarity.

--- lib/mail_queue_processor_control.rb
+++ lib/mail_queue_processor_control.rb
@@ -5,7 +5,7 @@ require File.join(File.dirname(__FILE__), 'daemons_extension')
 ENV['RAILS_ENV'] ||= 'development'
  
 options = {
-  :app_name  => 'processor',
+  :app_name  => 'mail_queue_processor',
   :dir_mode  => :script,
   :dir       => '../log',
   :backtrace => true,
@@ -13,4 +13,4 @@ options = {
   :monitor   => true
 }
  
-Daemons.run(File.join(File.dirname(__FILE__), 'processor.rb'), options)
+Daemons.run(File.join(File.dirname(__FILE__), 'mail_queue_processor.rb'), options)
